da7dfdab0389911b9cd0b9c5af37e6a5bfd7317c
clem-kay <ckoomson75@gmail.com>
2021-03-11 17:15:02 +0000

    first commit

---
+++ src/main/java/com/example/clientConnectivity/ClientConnectivityApplication.java
@@ -0,0 +1,13 @@
+package com.example.clientConnectivity;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ClientConnectivityApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ClientConnectivityApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/clientConnectivity/ClientConnectivityApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.clientConnectivity;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ClientConnectivityApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
